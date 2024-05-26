c9bd6a9c0b576507e9ca778803c43818e796bec5
unknown <kayv@hotmail.fr>
2024-05-26 09:00:21 +0200

    route dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import './dashboard.css'
 export default function Dashboard(){
     return(
-        <section className="dashboard"></section>
+        <section className="dashboard">
+            
+        </section>
     )
 }
